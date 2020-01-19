aa41cf0e812516a192c002febe8ba15532beeb51
Srdjan Stankov <srki.miki@gmail.com>
2020-01-20 00:31:41 +0100

    Added Buttons to canvases

---
+++ Assets/BtnHoverColor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class BtnHoverColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Color NormalColor = Color.white;
+    public Color HoverColor;
+
+    private TMP_Text text;
+
+    void Start()
+    {
+        text = GetComponentInChildren<TMP_Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = HoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = NormalColor;
+    }
+}
