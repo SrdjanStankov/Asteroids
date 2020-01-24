620625584b1137a24299b40d7605e0d36ae3d121
Srdjan Stankov <srki.miki@gmail.com>
2020-01-25 00:16:46 +0100

    Created Speed up collectable

---
+++ Assets/Scripts/SpeedUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SpeedUp : Pickupable
+{
+    private SpaceshipMovement spaceshipMovement;
+
+    protected override void OnEndOfDuration()
+    {
+        try
+        {
+            spaceshipMovement.FlightSpeed /= 2;
+            spaceshipMovement.RotationSpeed /= 2;
+        }
+        catch (System.Exception) { } // if spaceship is destroyed before duration expires
+        Destroy(gameObject);
+    }
+
+    protected override void OnPickup(Collider2D collision)
+    {
+        spaceshipMovement = collision.GetComponent<SpaceshipMovement>();
+        spaceshipMovement.FlightSpeed *= 2;
+        spaceshipMovement.RotationSpeed *= 2;
+    }
+}
